8b5525e6695577a30106f72036e2ed64b4aba8ed
kenkh <kenkh@LAPTOP-C2IDUM6L.lan>
2020-09-24 21:41:23 +0100

    Jira security worked

--- springbootken/src/main/java/com/example/ken/JiraSecurityConnector.java
+++ springbootken/src/main/java/com/example/ken/JiraSecurityConnector.java
@@ -1,10 +1,15 @@
 package com.example.ken;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,8 +22,9 @@ import org.springframework.security.core.GrantedAuthority;
 
 @Configuration
 @EnableWebSecurity
-public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+public class JiraSecurityConnector extends WebSecurityConfigurerAdapter {
 	
+	private static final String SYSTEM_SITE = "kenkhwong.atlassian.net";
 	private static final String SYSTEM_USER = "st@kenkhwong.atlassian.net";
 	
 	@Autowired
@@ -28,12 +34,32 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(new AuthenticationProvider() {
         	@Override
         	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        		
         		if (!SYSTEM_USER.equals(authentication.getPrincipal()))
-        			throw new BadCredentialsException("Not a system account");
+        			throw new BadCredentialsException("Not a system user");
         		
-        		
-        		return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(),
-        				authentication.getCredentials(),new HashSet<GrantedAuthority>());
+        		String url = "https://" + SYSTEM_SITE + "/rest/servicedeskapi/servicedesk";
+        		String encodedCred = Base64.getEncoder().encodeToString((authentication.getPrincipal()
+        				+ ":" + authentication.getCredentials()).getBytes());
+
+        		try {
+        			HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        			con.setRequestProperty("Authorization", "Basic " + encodedCred);
+        			con.setRequestMethod("GET");
+
+        			int jiraResponseCode = con.getResponseCode();
+        			
+        			switch (jiraResponseCode) {
+        			case 200 : //successful
+        				return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(),
+                				authentication.getCredentials(),new HashSet<GrantedAuthority>());
+        			default :
+        				throw new AuthenticationServiceException("Jira response is " + jiraResponseCode);
+        			}
+
+        		} catch (IOException e) {
+        			throw new AuthenticationServiceException(e.toString(), e);
+        		}
         	}
 
         	@Override
