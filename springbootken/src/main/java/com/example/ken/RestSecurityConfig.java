7954fa7b8fe25064970a00328aaa95b037492757
kenkh <kenkh@LAPTOP-C2IDUM6L.lan>
2020-09-24 02:09:27 +0100

    Jira security

--- springbootken/src/main/java/com/example/ken/RestSecurityConfig.java
+++ springbootken/src/main/java/com/example/ken/RestSecurityConfig.java
@@ -1,10 +1,22 @@
 package com.example.ken;
 
+import java.util.HashSet;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +25,31 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final String SYSTEM_USER = "st@kenkhwong.atlassian.net";
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.authenticationProvider(new AuthenticationProvider() {
+        	@Override
+        	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        		if (!SYSTEM_USER.equals(authentication.getPrincipal()))
+        			throw new BadCredentialsException("Not a system account");
+        		
+        		
+        		return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(),
+        				authentication.getCredentials(),new HashSet<GrantedAuthority>());
+        	}
+
+        	@Override
+        	public boolean supports(Class<?> authentication) {
+        		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        	}
+        });
+    }
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -21,7 +58,7 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated()
 				.and().httpBasic();
 	}
-
+/*
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
@@ -32,5 +69,5 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
-	}
+	}*/
 }
